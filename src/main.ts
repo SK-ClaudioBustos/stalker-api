7d79df70cedde692129089044bbb04ca8d88c505
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-08-20 19:06:34 -0300

    feat: add class-transformer and class-validator dependencies, implement IsObjectId decorator, and configure global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,13 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173'
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const options = new DocumentBuilder()
     .setTitle('STALKER REST API')
