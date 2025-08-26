f1c3ab3755c23de1df50054ecb76a7671b8cd0e0
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-08-26 10:04:17 -0300

    feat: update CORS configuration with environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:5173'
+    origin: process.env.FRONTEND_URL,
   });
-  Logger.verbose('Para acceder al playground entre a http://localhost:3000/graphql');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
