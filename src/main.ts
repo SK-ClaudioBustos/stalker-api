d611dfdbc66eb25dd28cd0642f5693b2afb58344
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-09-05 09:05:46 -0300

    feat: remove swagger library

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CustomExceptionHandlerFilter } from './filters/custom-exception-handler.filter';
 
@@ -18,15 +17,6 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new CustomExceptionHandlerFilter());
-
-  const options = new DocumentBuilder()
-    .setTitle('STALKER REST API')
-    .setDescription('API REST para el sitio web de mods traducidos de STALKER')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
